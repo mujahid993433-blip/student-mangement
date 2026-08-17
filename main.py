8d538e7d6aa84b6f4f83b0e1de69326c1cacf114
mujahid993433-blip <mujahid993433$>
2026-08-17 00:59:02 -0700

    added manue for the program with the help of if condition

--- main.py
+++ main.py
@@ -6,3 +6,17 @@ def delete():
     pass
 def update():
     pass
+try:
+    n = int(input("enter the number for your choice"))
+except ValueError:
+    print("enter the number")
+if n == 1:
+    add()
+elif n == 2:
+    search()
+elif n == 3:
+    delete()
+elif n== 4:
+    update()
+else:
+    print("wrong choice")
